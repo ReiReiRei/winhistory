538994fb324ed65e932dc57eb44820cbd4a4064f
ReiReiRei <frostball@gmail.com>
2014-05-10 19:28:57 +0400

    Множество изменени,теперь есть первая тестовая программа

---
+++ test_program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib;
+
+namespace test_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid my = new Guid();
+
+            var  messages = WinHistory.Login(my, "ТроЛолЛо");
+            messages.Send("ГОГОГО");
+
+        }
+    }
+}
